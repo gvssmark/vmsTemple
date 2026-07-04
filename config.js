3c1a245b6d0e0509c7f9823f55fe0cb4f43fd1fc
gvssmark <83894487+gvssmark@users.noreply.github.com>
2026-07-04 09:27:44 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,57 @@
+/*
+=========================================================
+Temple Website Framework
+Configuration
+=========================================================
+*/
+
+const CONFIG = {
+
+    // Google Apps Script API
+    API_URL:
+    "https://script.google.com/macros/s/AKfycbyf_tPb1L23wvcBRH3t_MjDFI0xDDUVC-uU7ADMU197RSYoiD9Fz55pvsQFV2TLNoOG/exec",
+
+    // Website
+
+    APP_NAME:
+    "Temple Website",
+
+    CACHE_NAME:
+    "TempleCache",
+
+    CACHE_TIME:
+    60 * 60 * 1000,      // 1 Hour
+
+    IMAGE_PLACEHOLDER:
+    "https://placehold.co/600x400?text=Temple",
+
+    DEFAULT_BANNER:
+    "https://placehold.co/1200x600?text=Temple+Banner"
+
+};
+
+const LABELS = {
+
+    announcements: "ప్రకటనలు",
+
+    festivals: "పండుగలు",
+
+    darshan: "దర్శన సమయములు",
+
+    poojas: "పూజలు",
+
+    sthala: "స్థల పురాణం",
+
+    deity: "దేవత చిత్రాలు",
+
+    temple: "గుడి చిత్రాలు",
+
+    miracles: "దేవత మహిమలు",
+
+    experiences: "భక్తుల అనుభవాలు",
+
+    donors: "దాతల వివరాలు",
+
+    contact: "సంప్రదించండి"
+
+};
